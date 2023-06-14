0cb86ba4000a4cb344d15c5cf735454229af6101
De4oult <timofej.dimitrov@yandex.ru>
2023-06-14 19:23:46 +0300

    editor update, validator

--- src/interface/editor.py
+++ src/interface/editor.py
@@ -2,17 +2,19 @@ from modules.runner import execute
 from colorama       import Fore, Style
 
 def editor() -> None:
-    line: int = 0 # current line
+    line: int = -1 # current line
     while line < 1000:
         try:
-            res, err = execute('<interpreter>', input("{:03d} ~ ".format(line)))
+            line += 1
+            code     = input('{:03d} ~ '.format(line))
+            if code.strip() == '': continue
+
+            res, err = execute('<interpreter>', code)
             
             if err:   print(err.as_string())
             elif res: 
                 if len(res.elements) == 1: print(Fore.GREEN + repr(res.elements[0]) + Style.RESET_ALL)
                 else:                      print(Fore.GREEN + repr(res)             + Style.RESET_ALL)
-            
-            line += 1
 
         except KeyboardInterrupt:
             exit()
